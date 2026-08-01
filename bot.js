8140c666f2bc1b24f245e6121cf0e1b696a5748e
Rahmi <samatya231@gmail.com>
2026-08-01 11:35:41 +0300

    False positive engel kelimeleri kaldirildi

--- bot.js
+++ bot.js
@@ -52,9 +52,9 @@ const UZUN_MOLA_MAX    = 7500;
 const ENGEL_ISARETLERI = [
     'erişim engellendi', 'erisim engellendi',
     'access denied', 'blocked', 'too many requests',
-    'rate limit', 'captcha', 'robot', 'ban',
-    '403', '429', '503',
+    'rate limit', 'captcha', 'robot değilim', 'not a robot',
     'unusual traffic', 'suspicious activity',
+    'bağlantıda hata oluştu', 'baglantida hata olustu',
 ];
 
 const KALP_SVG_PATH = 'M480-147q-14 0-28.5-5T426-168l-69-63q-106-97-191.5-192.5T80-634q0-94 63-157t157-63q53 0 100 22.5t80 61.5q33-39 80-61.5T660-854q94 0 157 63t63 157q0 115-85 211T602-230l-68 62q-11 11-25.5 16t-28.5 5Zm-38-543q-29-41-62-62.5T300-774q-60 0-100 40t-40 100q0 52 37 110.5T285.5-410q51.5 55 106 103t88.5 79q34-31 88.5-79t106-103Q726-465 763-523.5T800-634q0-60-40-100t-100-40q-47 0-80 21.5T518-690q-7 10-17 15t-21 5q-11 0-21-5t-17-15Zm38 189Z';
